e42f6916542159c360c4b4a32fac4b53f196dab0
n <2585803+tt5@users.noreply.github.com>
2022-04-23 13:52:01 +0200

    small change (testing)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 console.log("Listening on http://localhost:8000");
 serve((_req) => {
-  return new Response("Hello World!", {
+  return new Response("Hello2", {
     headers: { "content-type": "text/plain" },
   });
 });
